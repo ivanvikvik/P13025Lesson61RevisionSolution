5a5adfc8517677b173d9c8b062eea707b499c62b
ivanvikvik <ivanvikvik@gmail.com>
2026-05-06 18:57:34 +0300

    Update solution

---
+++ Task01/util.h
@@ -0,0 +1,7 @@
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include "logic.h"
+using namespace std;
+
+void print_test(int a, int b, int c, int ea, int eb, int ec, string test_name);
